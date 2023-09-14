4690dc0eb30d1dd412636ea687df234c2fa4103f
Saborowski, Kai <kai.saborowski@hdi.de>
2023-09-14 12:54:03 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # virtual-threads-example-java21
+
+#### simply demonstrating the use of Java Virtual Threads coming with Java 21.
+
+**Prerequisites:**
+
+* [Java 20 with --enable-preview / 21](https://openjdk.net/)
+* [Apache Maven](https:http://maven.apache.org/)
+
+#### How to build and run
+
+Type
+
+```bash
+mvn verify
+```
+to build and run the application on your local environment.
---
+++ src/test/java/de/ksbrwsk/FixedThreadPoolTaskTest.java
@@ -0,0 +1,36 @@
+package de.ksbrwsk;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FixedThreadPoolTaskTest {
+
+    @Test
+    void execute() throws Exception {
+        try (ExecutorService executor = Executors.newFixedThreadPool(100)) {
+            List<Task> tasks = new ArrayList<>();
+            for (int i = 0; i < 1_000; i++) {
+                tasks.add(new Task(i));
+            }
+            long time = System.currentTimeMillis();
+            List<Future<Integer>> futures = executor.invokeAll(tasks);
+            long sum = 0;
+            for (Future<Integer> future : futures) {
+                sum += future.get();
+            }
+            time = System.currentTimeMillis() - time;
+            System.out.println("****************************************************");
+            System.out.println("sum = " + sum + "; time = " + time + " ms");
+            System.out.println("****************************************************\n\n");
+            executor.shutdown();
+            assertTrue(time >= 10000);
+        }
+    }
+}
